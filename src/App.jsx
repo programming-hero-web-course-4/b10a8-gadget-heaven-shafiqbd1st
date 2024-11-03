a9d0d1765ea6763e7e4f4c953adb2dc6455320c9
shafiqbd1st <shafiqbd1st@gmail.com>
2024-11-03 22:59:05 +0600

    complete all setup file

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-3xl'>This is app components</h1>
+      <button className="btn btn-primary">Click Me</button>
+    </div>
+  )
+}
+
+export default App
